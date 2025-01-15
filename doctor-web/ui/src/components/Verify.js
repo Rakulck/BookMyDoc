6fe62f28d75f4b2e6cd1e10950e77ff53776d8c3
Rakul CK <77563090+Rakulck@users.noreply.github.com>
2025-01-15 17:32:02 +0600

    Update Verify.js

--- doctor-web/ui/src/components/Verify.js
+++ doctor-web/ui/src/components/Verify.js
@@ -72,7 +72,7 @@ const Verify = () => {
     }
   };
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  /* eslint-disable-next-line react-hooks/exhaustive-deps */
   useEffect(() => {
     if (isAuthenticated) {
       navigate('/');
