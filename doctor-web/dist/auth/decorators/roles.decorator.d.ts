048f36bfe8b18a5f683486ecaf2cdd2cd6c8c1d4
CREvolt <info@crevolt.com>
2025-08-13 15:44:51 -0400

    deploy

--- doctor-web/dist/auth/decorators/roles.decorator.d.ts
+++ doctor-web/dist/auth/decorators/roles.decorator.d.ts
@@ -1,4 +1,4 @@
 import { IRole } from '@app/common/types/type';
 export declare const ROLES_KEY = "roles";
 export declare const Roles: (...roles: IRole[]) => import("@nestjs/common").CustomDecorator<string>;
-export declare const RoleGuard: (...roles: IRole[]) => <TFunction extends Function, Y>(target: object | TFunction, propertyKey?: string | symbol, descriptor?: TypedPropertyDescriptor<Y>) => void;
+export declare const RoleGuard: (...roles: IRole[]) => <TFunction extends Function, Y>(target: TFunction | object, propertyKey?: string | symbol, descriptor?: TypedPropertyDescriptor<Y>) => void;
